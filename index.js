83910f4f5107ec5d300a35db4ae4962c15f934e5
Chumki khatun <chumkikhatun370@gmail.com>
2024-03-10 08:25:51 +0600

    connect mongodb

--- index.js
+++ index.js
@@ -4,6 +4,7 @@ require('dotenv').config();
 const cors= require('cors');
 const jwt =require('jsonwebtoken');
 const cookieParser = require('cookie-parser');
+const { MongoClient, ServerApiVersion } = require('mongodb');
 const port= process.env.PORT || 5000;
 // middleware
 const corsOptions={
@@ -14,6 +15,33 @@ const corsOptions={
 app.use(cors(corsOptions))
 app.use(express.json());
 app.use(cookieParser())
+
+
+
+
+
+// Create a MongoClient with a MongoClientOptions object to set the Stable API version
+const client = new MongoClient(process.env.DB_URL, {
+  serverApi: {
+    version: ServerApiVersion.v1,
+    strict: true,
+    deprecationErrors: true,
+  }
+});
+
+async function run() {
+  try {
+    
+    // Send a ping to confirm a successful connection
+    await client.db("admin").command({ ping: 1 });
+    console.log("Pinged your deployment. You successfully connected to MongoDB!");
+  } finally {
+    // Ensures that the client will close when you finish/error
+    // await client.close();
+  }
+}
+run().catch(console.dir);
+
 
 app.get('/',(req,res)=>{
     res.send('hello shopsy')
